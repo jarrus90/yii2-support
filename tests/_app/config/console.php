e90a240ae4ebc68306d55978b52d8dba1b12852b
Vladimir Stovbchatij <unreality90@mail.ru>
2016-09-22 15:24:34 +0300

    Tests update

--- tests/_app/config/console.php
+++ tests/_app/config/console.php
@@ -3,6 +3,11 @@
 return [
     'id' => 'yii2-test-console',
     'basePath' => dirname(__DIR__),
+    'controllerMap' => [
+        'migrate' => [
+            'class' => 'jarrus90\Core\Console\MigrateController',
+        ],
+    ],
     'aliases' => [
         '@jarrus90/Support' => dirname(dirname(dirname(__DIR__))),
         '@tests' => dirname(dirname(__DIR__)),
