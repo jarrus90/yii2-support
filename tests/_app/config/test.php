3307978b8240ac4086d1022e7b9b6665a1cb0615
Vladimir Stovbchatij <unreality90@mail.ru>
2016-09-22 16:05:33 +0300

    Fix tests

--- tests/_app/config/test.php
+++ tests/_app/config/test.php
@@ -5,6 +5,8 @@ return [
     'basePath' => dirname(__DIR__),
     'language' => 'en-US',
     'aliases' => [
+        '@jarrus90/Multilang' => VENDOR_DIR . '/jarrus90/yii2-multilang',
+        '@jarrus90/User' => VENDOR_DIR . '/jarrus90/yii2-user',
         '@jarrus90/Support' => dirname(dirname(dirname(__DIR__))),
         '@tests' => dirname(dirname(__DIR__)),
         '@vendor' => VENDOR_DIR,
