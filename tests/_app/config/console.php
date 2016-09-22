ed8dc2c665f543be64e2985fc3d1e4d7a9197b95
Vladimir Stovbchatij <unreality90@mail.ru>
2016-09-22 18:11:59 +0300

    Fix tests

--- tests/_app/config/console.php
+++ tests/_app/config/console.php
@@ -2,7 +2,7 @@
 
 return [
     'id' => 'yii2-support-console',
-    'basePath' => dirname(__DIR__),
+    'basePath' => dirname(dirname(__FILE__)),
     'bootstrap' => [
         'jarrus90\Multilang\Bootstrap',
         'jarrus90\User\Bootstrap',
@@ -15,7 +15,7 @@ return [
         ],
     ],
     'aliases' => [
-        '@baseApp' => dirname(dirname(__FILE__)),
+        '@baseApp' => dirname(__DIR__),
         '@jarrus90/Multilang' => VENDOR_DIR . '/jarrus90/yii2-multilang',
         '@jarrus90/User' => VENDOR_DIR . '/jarrus90/yii2-user',
         '@jarrus90/Support' => dirname(dirname(dirname(__DIR__))),
